3f90bbafee067511cac85bbd5a2fb670e0cbe10c
Marta Mora <41650008k@institutmontilivi.cat>
2019-10-01 16:36:54 +0200

    AplicacioJocs

---
+++ Exercici16/Exercici16/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercici16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static public void Menu()
+        {
+            Console.WriteLine("Llista Jocs: ");
+            Console.WriteLine();
+            Console.WriteLine("1 - Sims");
+            Console.WriteLine("2 - Sims2");
+            Console.WriteLine("3 - Sims3");
+            Console.WriteLine("4 - Sims4");
+
+            int opcio = int.Parse(Console.ReadLine());
+            if (opcio>0 && opcio<5)
+            {
+                switch (opcio)
+                {
+                    case 1:
+                        Sims();
+                        break;
+
+                    case 2:
+                        Sims2();
+                        break;
+
+                    case 3:
+                        Sims3();
+                        break;
+                    case 4:
+                        Sims4();
+                        break;
+                }
+            }
+            
+        }
+        static public void Sims()
+        {
+            Console.WriteLine("Hola, Soc els sims");
+        }
+        static public void Sims2()
+        {
+            Console.WriteLine("Hola, Soc els sims2");
+        }
+        static public void Sims3()
+        {
+            Console.WriteLine("Hola, Soc els sims3");
+        }
+        static public void Sims4()
+        {
+            Console.WriteLine("Hola, Soc els sims4");
+        }
+    }
+}
